f3ff62380878564d720816bf2fcac01a0c689da0
lczfdd <danialafiqaziz11@gmail.com>
2026-05-29 05:19:32 +0800

    updated list and view

--- Assg1/OrderList.php
+++ Assg1/OrderList.php
@@ -1,6 +1,6 @@
 <!--
 Assignment 1, SCSM2223-25262 (OrderList.php)
-Group Name: ???
+Group Name: Strange
 -->
 <?php require 'libs/authpage.php'; ?>
 <?php require 'libs/db_connect_PDO.php'; ?>
@@ -8,15 +8,30 @@ Group Name: ???
 $keywordOrder = "";
 $sortOrder = "datetime desc"; // default sort order is datetime, descending 
 
+if (isset($_POST['keywordOrder'])) {
+  $keywordOrder = $_POST['keywordOrder'];
+}
+if (isset($_GET['sortOrder'])) {
+  $sortOrder = $_GET['sortOrder'];
+}
+
 // Assume that no field is being selected to sort the menu rows
-$sortFields = ['datetime desc'=>'', 'name'=>'', 'delivery'=>'', 'phone'=>''];
+$sortFields = ['datetime desc' => '', 'name' => '', 'delivery' => '', 'phone' => ''];
 
 // Determine which field is selected for sorting
 $sortFields[$sortOrder] = "***";
 
 // Construct the SQL to list customer'a orders based on $keywordOrder and $sortOrder variable
-$stmt = $pdo->prepare("SELECT * FROM orders ORDER BY $sortOrder");
-  
+$stmt = $pdo->prepare("SELECT orders.*, users.name, users.phone, (SELECT SUM(qty) 
+FROM order_menus WHERE order_id = orders.id) as total_qty 
+FROM orders JOIN users ON orders.user_id = users.id 
+WHERE users.name LIKE '%$keywordOrder%' 
+OR users.phone LIKE '%$keywordOrder%' 
+OR orders.delivery LIKE '%$keywordOrder%' 
+OR orders.status LIKE '%$keywordOrder%' 
+OR orders.datetime LIKE '%$keywordOrder%' 
+ORDER BY $sortOrder");
+
 try {
   $stmt->execute();
   //echo $stmt->debugDumpParams();
@@ -28,96 +43,99 @@ try {
 
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Tasty Bites - Delete Menu</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="main_style.css"  rel="stylesheet" type="text/css">
+  <title>Tasty Bites - Delete Menu</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="main_style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 
-<!-- Main Layout Table -->
-<table border="0" width="100%">
-  <!-- Header -->
-  <tr>
-    <td align="center">
-      <?php include 'libs/header.php'; ?>
-    </td>
-  </tr>
+  <!-- Main Layout Table -->
+  <table border="0" width="100%">
+    <!-- Header -->
+    <tr>
+      <td align="center">
+        <?php include 'libs/header.php'; ?>
+      </td>
+    </tr>
 
-  <!-- Navigation -->
-  <tr>
-    <td align="center">
-      <?php include 'libs/navigation.php'; ?>
-    </td>
-  </tr>
+    <!-- Navigation -->
+    <tr>
+      <td align="center">
+        <?php include 'libs/navigation.php'; ?>
+      </td>
+    </tr>
 
-  <!-- Content Row -->
-  <tr>
-    <!-- Order Section -->
-    <td>
-      <h2>List Customer Order</h2>
-      <div style="text-align: right">
-        <form action="OrderList.php" method="POST">
-          <b>Search:</b> <input type="text" name="keywordOrder" value="<?= $keywordOrder ?>"> <button type="submit">Submit</button>
-        </form>
-      </div>
-      <br>
-      <table border="1" width="100%">
-        <tr>
-          <th>No.</th>
-          <th>ID</th>
-          <th><a href="OrderList.php?sortOrder=datetime+desc">Date-Time<?= $sortFields['datetime desc'] ?></a></th>
-          <th><a href="OrderList.php?sortOrder=name">Name <?= $sortFields['name'] ?></a></th>
-          <th><a href="OrderList.php?sortOrder=delivery">Delivery<?= $sortFields['delivery'] ?></a></th>
-          <th><a href="OrderList.php?sortOrder=phone">Phone<?= $sortFields['phone'] ?></a></th>
-          <th>Menu Ordered</th>
-          <th><a href="OrderList.php?sortOrder=status">Status</a></th>
-          <th>Operations</th>
-        </tr>
-<?php 
-$no = 1;
-while ($row = $stmt->fetch()) { 
-?>
-        <tr>
-          <td align="center"><?= $no ?></td>
-          <td align="center"><?= $row['id'] ?></td>
-          <td align="center"><?= $row['datetime'] ?></td>
-          <td>???</td>
-          <td><?= $row['delivery'] ?></td>
-          <td>???</td>
-          <td align="center">???</td>
-          <td align="center"><?= $row['status'] ?></td>
-          <td align="center">
-            <a href="OrderView.php?id=<?= $row['id'] ?>&task=">View</a>
-          </td>
-        </tr>
-<?php 
-  $no++;
-} 
-?>
-        <tr>
-          <td align="center">...</td>
-          <td align="center">...</td>
-          <td align="center">...</td>
-          <td>...</td>
-          <td>...</td>
-          <td>...</td>
-          <td align="center">...</td>
-          <td align="center">...</td>
-          <td align="center">...</td>
-        </tr>
-      </table>
-    </td>
-  </tr>
+    <!-- Content Row -->
+    <tr>
+      <!-- Order Section -->
+      <td>
+        <h2>List Customer Order</h2>
+        <div style="text-align: right">
+          <form action="OrderList.php" method="POST">
+            <b>Search:</b> <input type="text" name="keywordOrder" value="<?= $keywordOrder ?>"> <button
+              type="submit">Submit</button>
+          </form>
+        </div>
+        <br>
+        <table border="1" width="100%">
+          <tr>
+            <th>No.</th>
+            <th>ID</th>
+            <th><a href="OrderList.php?sortOrder=datetime+desc">Date-Time<?= $sortFields['datetime desc'] ?></a></th>
+            <th><a href="OrderList.php?sortOrder=name">Name <?= $sortFields['name'] ?></a></th>
+            <th><a href="OrderList.php?sortOrder=delivery">Delivery<?= $sortFields['delivery'] ?></a></th>
+            <th><a href="OrderList.php?sortOrder=phone">Phone<?= $sortFields['phone'] ?></a></th>
+            <th>Menu Ordered</th>
+            <th><a href="OrderList.php?sortOrder=status">Status</a></th>
+            <th>Operations</th>
+          </tr>
+          <?php
+          $no = 1;
+          while ($row = $stmt->fetch()) {
+            ?>
+            <tr>
+              <td align="center"><?= $no ?></td>
+              <td align="center"><?= $row['id'] ?></td>
+              <td align="center"><?= $row['datetime'] ?></td>
+              <td><?= $row['name'] ?></td>
+              <td><?= $row['delivery'] ?></td>
+              <td><?= $row['phone'] ?></td>
+              <td align="center"><?= $row['total_qty'] ?></td>
+              <td align="center"><?= $row['status'] ?></td>
+              <td align="center">
+                <a href="OrderView.php?id=<?= $row['id'] ?>&task=">View</a>
+              </td>
+            </tr>
+            <?php
+            $no++;
+          }
+          ?>
+          <tr>
+            <td align="center">...</td>
+            <td align="center">...</td>
+            <td align="center">...</td>
+            <td>...</td>
+            <td>...</td>
+            <td>...</td>
+            <td align="center">...</td>
+            <td align="center">...</td>
+            <td align="center">...</td>
+          </tr>
+        </table>
+      </td>
+    </tr>
 
-  <!-- Footer -->
-  <tr>
-    <td align="center">
-      <?php include 'libs/footer.php'; ?>
-    </td>
-  </tr>
-</table>
+    <!-- Footer -->
+    <tr>
+      <td align="center">
+        <?php include 'libs/footer.php'; ?>
+      </td>
+    </tr>
+  </table>
 
 </body>
+
 </html>
--- Assg1/OrderView.php
+++ Assg1/OrderView.php
@@ -7,31 +7,40 @@ Group Name: ???
 <?php
 $changeStatus = "";
 if (isset($_GET['changeStatus'])) {
-  $changeStatus  = $_GET['changeStatus'];
+  $changeStatus = $_GET['changeStatus'];
 }
 
 // Associatve array to conveniently change the order status
 $statusMap = [
-  'New' => ['prev'=>'New', 'next'=>'Prepare' ],
-  'Prepare' => ['prev'=>'New', 'next'=>'Delivered' ],
-  'Delivered' => ['prev'=>'Prepare', 'next'=>'Delivered' ],
+  'New' => ['prev' => 'New', 'next' => 'Prepare'],
+  'Prepare' => ['prev' => 'New', 'next' => 'Delivered'],
+  'Delivered' => ['prev' => 'Prepare', 'next' => 'Delivered'],
 ];
 
 // Construct the SQL to get user info and ordered menus from the database
-//$stmtOrder = $pdo->prepare("SELECT ???");
-                           
-//$stmtMenu = $pdo->prepare("SELECT ???");
+$stmtOrder = $pdo->prepare("SELECT orders.*, users.name, users.phone FROM orders JOIN users ON orders.user_id = users.id WHERE orders.id = :id");
+
+$stmtMenu = $pdo->prepare("SELECT order_menus.qty, menus.name, menus.price FROM order_menus JOIN menus ON order_menus.menu_id = menus.id WHERE order_menus.order_id = :id");
 
 // Construct the SQL to get user info and ordered menus from the database                           
 $stmtStatusSave = $pdo->prepare("UPDATE orders SET status=:status WHERE id=:id");
-  
+
 try {
-  // ???
-  
+  $stmtOrder->execute(['id' => $_GET['id']]);
+  $order = $stmtOrder->fetch();
+
+  if ($changeStatus != "") {
+    $newStatus = $statusMap[$order['status']][$changeStatus];
+    $stmtStatusSave->execute(['status' => $newStatus, 'id' => $_GET['id']]);
+    $order['status'] = $newStatus;
+  }
+
+  $stmtMenu->execute(['id' => $_GET['id']]);
+
   //echo $stmtOrder->debugDumpParams();
   //echo $stmtMenu->debugDumpParams();
   //echo $stmtStatusSave->debugDumpParams();
-  
+
 } catch (PDOException $ex) {
   echo "Database Error: " . $ex->getMessage();
 }
@@ -40,103 +49,112 @@ try {
 
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Tasty Bites - Delete Menu</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="main_style.css"  rel="stylesheet" type="text/css">
+  <title>Tasty Bites - Delete Menu</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="main_style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 
-<!-- Main Layout Table -->
-<table border="0" width="100%">
-  <!-- Header -->
-  <tr>
-    <td align="center">
-      <?php include 'libs/header.php'; ?>
-    </td>
-  </tr>
+  <!-- Main Layout Table -->
+  <table border="0" width="100%">
+    <!-- Header -->
+    <tr>
+      <td align="center">
+        <?php include 'libs/header.php'; ?>
+      </td>
+    </tr>
 
-  <!-- Navigation -->
-  <tr>
-    <td align="center">
-      <?php include 'libs/navigation.php'; ?>
-    </td>
-  </tr>
+    <!-- Navigation -->
+    <tr>
+      <td align="center">
+        <?php include 'libs/navigation.php'; ?>
+      </td>
+    </tr>
 
-  <!-- Content Row -->
-  <tr>
-    <!-- Order Section -->
-    <td>
-      <h2>View Customer Order</h2>
-      <table cellpadding="3">
-        <tr>
-          <th align="right">Name: </th>
-          <td>???</td>
-        </tr>
-        <tr>
-          <th align="right">Phone Number: </th>
-          <td>???</td>
-        </tr>
-        <tr>
-          <th align="right">Delivery Option: </th>
-          <td>???</td>
-        </tr>
-        <tr>
-          <th align="right">Date-Time: </th>
-          <td>???</td>
-        </tr>
-        <tr>
-          <th align="right">Status: </th>
-          <td>
-            <a href="OrderView.php?changeStatus=prev&id=<?= $_GET['id'] ?>">&lt;&lt;</a> 
-            [ ??? ] 
-            <a href="OrderView.php?changeStatus=next&id=<?= $_GET['id'] ?>">&gt;&gt;</a>
-          </td>
-        </tr>
-      </table>
-      <br>
-      <table border="0">
-        <tr>
-          <th align="center">&nbsp; QTY &nbsp;</th>
-          <th align="left">ITEM</th>
-          <th align="right">PRICE (RM)</th>
-        </tr>
-<?php 
-$totalPrice = 0;
-?>
-        <tr>
-          <td align="center">???</td>
-          <td>???</td>
-          <td align="right">???</td>
-        <tr>
-          <td colspan="3">&nbsp;</td>
-        </tr>
-        <tr>
-          <th colspan="2" align="left">SUBTOTAL</th>
-          <td align="right">RM <?= number_format($totalPrice, 2) ?></td>
-        </tr>
-        <tr>
-          <th colspan="2" align="left">SST (6%)</th>
-          <td align="right">RM <?= number_format($totalPrice * 0.06, 2) ?></td>
-        </tr> 
-        <tr>
-          <th colspan="2" align="left">TOTAL</th>
-          <td align="right">RM <?= number_format($totalPrice + $totalPrice * 0.06, 2) ?></td>
-        </tr> 
-      </table>
-      
-      <p><i>???</i></p>
-    </td>
-  </tr>
+    <!-- Content Row -->
+    <tr>
+      <!-- Order Section -->
+      <td>
+        <h2>View Customer Order</h2>
+        <table cellpadding="3">
+          <tr>
+            <th align="right">Name: </th>
+            <td><?= $order['name'] ?></td>
+          </tr>
+          <tr>
+            <th align="right">Phone Number: </th>
+            <td><?= $order['phone'] ?></td>
+          </tr>
+          <tr>
+            <th align="right">Delivery Option: </th>
+            <td><?= $order['delivery'] ?></td>
+          </tr>
+          <tr>
+            <th align="right">Date-Time: </th>
+            <td><?= $order['datetime'] ?></td>
+          </tr>
+          <tr>
+            <th align="right">Status: </th>
+            <td>
+              <a href="OrderView.php?changeStatus=prev&id=<?= $_GET['id'] ?>">&lt;&lt;</a>
+              [ <?= $order['status'] ?> ]
+              <a href="OrderView.php?changeStatus=next&id=<?= $_GET['id'] ?>">&gt;&gt;</a>
+            </td>
+          </tr>
+        </table>
+        <br>
+        <table border="0">
+          <tr>
+            <th align="center">&nbsp; QTY &nbsp;</th>
+            <th align="left">ITEM</th>
+            <th align="right">PRICE (RM)</th>
+          </tr>
+          <?php
+          $totalPrice = 0;
+          while ($menu = $stmtMenu->fetch()) {
+            $totalItem = $menu['qty'] * $menu['price'];
+            $totalPrice += $totalItem;
+            ?>
+            <tr>
+              <td align="center"><?= $menu['qty'] ?></td>
+              <td><?= $menu['name'] ?></td>
+              <td align="right"><?= number_format($totalItem, 2) ?></td>
+            </tr>
+            <?php
+          }
+          ?>
+          <tr>
+            <td colspan="3">&nbsp;</td>
+          </tr>
+          <tr>
+            <th colspan="2" align="left">SUBTOTAL</th>
+            <td align="right">RM <?= number_format($totalPrice, 2) ?></td>
+          </tr>
+          <tr>
+            <th colspan="2" align="left">SST (6%)</th>
+            <td align="right">RM <?= number_format($totalPrice * 0.06, 2) ?></td>
+          </tr>
+          <tr>
+            <th colspan="2" align="left">TOTAL</th>
+            <td align="right">RM <?= number_format($totalPrice + $totalPrice * 0.06, 2) ?></td>
+          </tr>
+        </table>
 
-  <!-- Footer -->
-  <tr>
-    <td align="center">
-      <?php include 'libs/footer.php'; ?>
-    </td>
-  </tr>
-</table>
+        <p><i><?= $order['comments'] ?></i></p>
+      </td>
+    </tr>
+
+    <!-- Footer -->
+    <tr>
+      <td align="center">
+        <?php include 'libs/footer.php'; ?>
+      </td>
+    </tr>
+  </table>
 
 </body>
+
 </html>
